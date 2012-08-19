b38bb8c258d885453aede1cfca9bab03f859dd2f
Teemu Erkkola <teemu.erkkola@iki.fi>
2012-08-19 20:59:57 +0300

    Make schema test load schema QMLON file, fix build process

--- test/schema.cpp
+++ test/schema.cpp
@@ -1,8 +1,13 @@
 #include "qmlon.h"
 #include "qmlonschema.h"
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 int main(int argc, char** argv)
 {
+  std::ifstream f("schema.qmlon");
+  qmlon::Value::Reference schema = qmlon::readValue(f);
 
+  return EXIT_SUCCESS;
 }
